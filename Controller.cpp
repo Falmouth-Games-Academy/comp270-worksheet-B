6cd5a7796f70563d3169b0edcb95a7179c1e9181
L-ux <Josh@Grillo.co.uk>
2019-10-27 15:28:01 +0000

    Added comments so I dont have to hurt my brain so hard looking back at it

--- Controller.cpp
+++ Controller.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "Controller.h"
 #include <math.h>
+// Josh G
+
 
 // Calculate the shot speed to hit the target, given the following information.
 //   tankPos: the position from which the bullet will be fired, in pixels relative to the top left corner of the screen
@@ -12,15 +14,15 @@ float Controller::calculateShotSpeed(const Vector2& tankPos, const Vector2& enem
 {
 	Vector2 displacement = enemyPos - tankPos;
 
-	float aaaaaangle = atanf(wind / gravity);
-	float graaaavity = sqrt(pow(wind, 2) + pow(gravity, 2));
+	float aaaaaangle = atanf(wind / gravity);  // angle between Y axis (gravity) and the gravity+wind combo vector
+	float graaaavity = sqrt(pow(wind, 2) + pow(gravity, 2)); // magnitude of gravity+wind combo vector
 
 	Vector2 displaaaaac = Vector2();
-	displaaaaac.x = (displacement.x * cos(aaaaaangle)) - (displacement.y * sin(aaaaaangle));
-	displaaaaac.y = (displacement.y * cos(aaaaaangle)) + (displacement.x * sin(aaaaaangle));
+	displaaaaac.x = (displacement.x * cos(aaaaaangle)) - (displacement.y * sin(aaaaaangle)); // calculating X distance in new co-ord plane where graaaavity is directly downwards (y +ve)
+	displaaaaac.y = (displacement.y * cos(aaaaaangle)) + (displacement.x * sin(aaaaaangle)); // calculating X distance in new co - ord plane
 	
-	float num = graaaavity * pow(displaaaaac.x, 2);
-	float den = ((displaaaaac.x * tan(shotAngleRadians - aaaaaangle)) + displaaaaac.y) * (pow(cos(shotAngleRadians - aaaaaangle),2));
+	float num = graaaavity * pow(displaaaaac.x, 2); // caluclating numerator of big equation, to make this all a tiny bit tidier
+	float den = ((displaaaaac.x * tan(shotAngleRadians - aaaaaangle)) + displaaaaac.y) * (pow(cos(shotAngleRadians - aaaaaangle),2)); // calculating denominator
 	
 	float shotSpeed = sqrt(0.5 * (num/den));
 	return shotSpeed;
@@ -35,14 +37,17 @@ float Controller::calculateShotSpeed(const Vector2& tankPos, const Vector2& enem
 float Controller::calculateShotAngle(const Vector2& tankPos, const Vector2& enemyPos, float shotSpeed, float gravity, float wind)
 {
 	Vector2 displacement = enemyPos - tankPos;
-	float aaaaaangle = atanf(wind / gravity);
-	float graaaavity = sqrt(pow(wind, 2) + pow(gravity, 2));
+	float aaaaaangle = atanf(wind / gravity);	// angle between Y axis (gravity) and the gravity+wind combo vector
+	float graaaavity = sqrt(pow(wind, 2) + pow(gravity, 2));	// magnitude of gravity+wind combo vector
 
 	Vector2 displac = Vector2();
-	displac.x = (displacement.x * cos(aaaaaangle)) - (displacement.y * sin(aaaaaangle));
-	displac.y = (displacement.y * cos(aaaaaangle)) + (displacement.x * sin(aaaaaangle)); 
+	displac.x = (displacement.x * cos(aaaaaangle)) - (displacement.y * sin(aaaaaangle)); // calculating X distance in new co-ord plane where graaaavity is directly downwards (y +ve)
+	displac.y = (displacement.y * cos(aaaaaangle)) + (displacement.x * sin(aaaaaangle)); // calculating X distance in new co - ord plane
 
-	float angle = aaaaaangle + atan(((pow(shotSpeed, 2)) + (sqrt(pow(shotSpeed, 4) - graaaavity * ((graaaavity*pow(displac.x, 2)) + (2 * -displac.y*pow(shotSpeed, 2)))))) / (graaaavity*displac.x));
+	float angle = aaaaaangle + atan(
+							((pow(shotSpeed, 2)) + 
+							(sqrt(pow(shotSpeed, 4) - graaaavity * ((graaaavity*pow(displac.x, 2)) + (2 * -displac.y*pow(shotSpeed, 2)))))) / 
+							(graaaavity*displac.x));		// it looks a slight bit less horrible when spaced like this.
 
 	return angle;
 }
--- Controller.h
+++ Controller.h
@@ -1,4 +1,5 @@
 #pragma once
+// Josh G
 
 #include "Vector2.h"
 
