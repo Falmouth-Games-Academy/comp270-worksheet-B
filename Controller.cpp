9a08b5cdea37fc88a4a853445faaf4fe91aa772b
Ashley Sands <ashsands1@amstrike.com>
2019-10-27 19:53:23 +0000

    cleaned up code :)

--- Controller.cpp
+++ Controller.cpp
@@ -44,63 +44,14 @@ float Controller::calculateVelocity(const Vector2& dif, float gravity, float ang
 {
 
 	//caculate the inital force.
-	//float force = dif.x * gravity / sinf(angle * 2.0f);	// good flat (correct)
-  //float force = (dif.x + (dif.x * (-dif.y / dif.x))) * gravity / sinf(angle * 2.0f);
-  //float force = (dif.x + (dif.x * tanf(-dif.y / dif.x) )) * gravity / sinf(angle * 2.0f);	// a lil closer? (higher hit rate)
-  //float force = (dif.x + (dif.x * (sinf(-dif.y / dif.x) * sinf(angle*2.f)) )) * gravity / sinf(angle * 2.0f); // (higher hit rate)
-  //float force = (dif.x + (dif.x * (tanf(-dif.y / dif.x) * sinf(angle*2.f)) )) * gravity / sinf(angle * 2.0f); // (higher hit rate)
-	//float force = (dif.x + ((dif.x/gravity) * ((tanf(dif.y / dif.x) * sinf(angle*2.0f))) )) * gravity / sinf(angle * 2.0f); // 
-	//float force = (powf(dif.x, 2.0f) * gravity) / ( dif.x * sinf(angle * 2.f) ) - ( 2.f * -dif.y *  powf(cosf( angle ), 2.f) );
 	float force = (dif.x * dif.x * gravity) /  (dif.x * sinf(angle * 2.f)  -  2.f * -dif.y * cosf( angle ) * cosf(angle) );
-	//float force = (dif.x + (dif.x * -sinf(dif.y / dif.x))) * gravity / sinf(angle * 2.0f);
-	//force -= (-dif.y / dif.x) * powf((0.5f * gravity), 2.0f) / tanf(angle * 2.f);
 
-	//force -= (-powf(dif.y, 2.0f)) / powf((0.5f * gravity), 2.0f) * tanf(angle * 2.f);
-
-	float w_force = 0;// -wind * cosf((-wind / (dif.x)) * 2.f); ;// dif.x * (-wind / gravity) / sinf(angle * 2.0f);
-
+	float w_force = 0;
 	force += w_force;
-
-	std::cout << "wind: " << wind << " w_wind " << w_force << " x: " << (w_force * cosf(angle)) << " y: " << (w_force * -sinf(angle)) << " w/g " << (-wind / gravity) << " || " << ((-wind * (-wind / (gravity)))) << std::endl;
-	std::cout << "force: " << force << " x: " << (force * cosf(angle)) << " y: " << (force * -sinf(angle)) << std::endl;
-
-
 
 	force = sqrtf(force);
 
-	Vector2 force_vect2 = force * Vector2(cosf(angle), -sinf(angle));
-	float flightLength = GetFlightLength(dif.y, force, angle, gravity);
-
-	//force -= (-dif.y + ((force_vect2.y/gravity)*force_vect2.y)) / (gravity) * ( sinf(angle * 2.0f) ) ;
-	//force += ((2.0f * dif.y) + ((force_vect2.y/gravity)*force_vect2.y)) / (gravity) * ( sinf(angle * 2.0f) ) ;
-	//force += (dif.y + (-force_vect2.y * flightLength));
-
-	std::cout << -dif.y << " * " << gravity << " / " << sinf(angle * 2.f) << std::endl;
-
-	std::cout << (flightLength) << " || " << force << std::endl;
-
-	/*
-	std::cout << (dif.x) * -wind / cos(angle * 2.0f) << std::endl;
-
-	//float force = sqrt( (gravity * pow(dif.x, 2.0f)) / sin(angle * 2.0f) / (dif.x+dif.y * (1.0f/tan(angle))));
-	Vector2 force_vect2 = force * Vector2(cosf(angle), -sinf(angle));
-
-	std::cout << "force / grav " << force_vect2.y << "/"<<gravity << "||" << (force_vect2.y * GetFlightLength(dif.y, force, angle, gravity)) <<"||" << (force_vect2.x * GetFlightLength(dif.y, force, angle, gravity)) << "=="<< dif.x << std::endl;
-	std::cout << "flight Length " << GetFlightLength(dif.y, force, angle, gravity) << std::endl;
-	std::cout << "wind " << (-wind * GetFlightLength(dif.y, force, angle, gravity)) << std::endl;
-	std::cout << "x" << force_vect2.x << " (finalVel) s " << (0.5f * (force_vect2.x + (force_vect2.x - (-wind * GetFlightLength(dif.y, force, angle, gravity)))) ) << std::endl;
-	std::cout << "x" << force_vect2.x << " (finalVel) s " << ( force_vect2.x - (0.5f * wind * pow(1.0f, 2)) ) << std::endl;
-	std::cout << "height dif " << dif.y << " x dif: "<< dif.x << " gravity " << gravity << " angle " << angle << std::endl;
-	std::cout << "force" << force << "fv.x: "<< force_vect2.x << "fv.y: " << force_vect2 .y << std::endl;
-	*/
-	//Vector2 velocity = force_vect2;
-	//float flightLength = GetFlightLength(dif.y, force, angle, gravity);
-
-	//velocity.x = (dif.x * flightLength);
-
-	//std::cout << "flight Length X " << velocity.y << std::endl;
-
-	return  force;// velocity.magnitude();
+	return  force;
 
 }
 
@@ -115,8 +66,6 @@ float Controller::GetFlightLength(float heightDif, float force, float angle, flo
 {
 
 	Vector2 velocity = force * Vector2(cosf(angle), sinf(angle));
-	
-	// float fall_len = 1 - (-heightDif / ((velocity.y / gravity) * velocity.y));
 
 	return (velocity.y / gravity) * 2.0f;// *(1 + fall_len);
 
